61d55133bda34c9ad4f43849c4a8a4edc0d8ecac
Nishchay D <dnishchaydml@gmail.com>
2025-07-03 22:45:19 +0530

    DeclaringVariable

---
+++ DeclaringVariable.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var variablename type = value
+	var str string = "Hello, Go!" // longhand declaration
+	str2 := "Hello, Go! 2"        // shorthand declaration
+	var a = 31
+
+	fmt.Println("1. method: ", str)
+	fmt.Println("2. method: ", str2)
+	fmt.Println("3. method: ", a)
+}
