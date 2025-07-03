147756d8765b050a5412e9d15390d602d98accbc
Nishchay D <dnishchaydml@gmail.com>
2025-07-03 22:25:58 +0530

    DataTypes

---
+++ DataTypes.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a bool = true
+	var b int = 42
+	var c string = "Hello, Go!"
+	var d float32 = 3.14
+
+	fmt.Println("Boolean:", a)
+	fmt.Println("Integer:", b)
+	fmt.Println("String:", c)
+	fmt.Println("Float:", d)
+}
